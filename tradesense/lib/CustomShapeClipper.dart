e8a726c2ff3039d3a266bb282200059634b72192
IKRAM <ikrambouleksaks@gmail.com>
2020-05-04 17:58:09 +0200

    modification fichier home

---
+++ tradesense/lib/CustomShapeClipper.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class CustomShapeClipper extends CustomClipper<Path>{
+  @override
+  getClip(Size size) {
+    // TODO: implement getClip
+    return null;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) => true;
+
+
+ }
--- tradesense/lib/home.dart
+++ tradesense/lib/home.dart
@@ -1,62 +1,41 @@
-import 'dart:ui';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'CustomShapeClipper.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      backgroundColor: Color.fromRGBO(67, 90, 177, 1.0),
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
+        backgroundColor: Color.fromRGBO(67, 90, 177, 1.0),
         elevation: 0,
-        leading: Icon(Icons.menu),
-
-
-
+        leading: Icon(Icons.menu,color: Colors.white,),
       ),
-      body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            children: <Widget>[
-              Container(
-
-                height: 100,
-                padding: EdgeInsets.all(10),
-                child: Image.asset('assets/logo.png', width: 200,height: 200,),
-                alignment: Alignment.center,
-                // height: 100,
-                //decoration: BoxDecoration(
-                //  image: DecorationImage(
-                //    image: AssetImage('assets/logo.png'),
-
-                //)
-                //),
-              ),
-              Container(
-                  child:  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        child:
-                        Text('hello'),
-                        color: Colors.white,
-                        alignment: Alignment.center,
-                        width: 300,
-                        height: 300,
-                      )
-                    ],
-                  )
-              ) //Spacer(),
-            ],
-          ),
-        ),
+      body: Column(
+        children: <Widget>[
+          HomeScreenTopPart()
+        ],
       ),
     );
   }
 }
+class HomeScreenTopPart extends StatefulWidget {
+  @override
+  _HomeScreenTopPartState createState() => _HomeScreenTopPartState();
+}
 
-
-
+class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        ClipPath(
+          clipper: CustomShapeClipper() ,
+           child: Container(height: 450.0, color: Color.fromRGBO(67, 90, 177, 1.0),
+          )
+        )
+      ],
+    );
+  }
+}
