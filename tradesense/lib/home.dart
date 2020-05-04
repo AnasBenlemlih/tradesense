c328f883ed9943c7389db7ee21e41c2ece808a87
AnasBenlemlih <anas.benlemlih@gmail.com>
2020-05-04 20:45:35 +0000

    menu

--- tradesense/lib/home.dart
+++ tradesense/lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'CustomShapeClipper.dart';
+import 'CustomListTitle.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -9,17 +10,53 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromRGBO(67, 90, 177, 1.0),
-        elevation: 0,
-        leading: Icon(Icons.menu,color: Colors.white,),
+        title: Text('TradeSense'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: <Color>[
+                  Color.fromRGBO(67, 90, 177, 1.0),
+                  Color.fromRGBO(67, 90, 177, 1.0)
+                ])),
+                child: Container(
+                  child: Column(
+                    children: <Widget>[
+                      Material(
+                        borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                        elevation: 15,
+                        child: Padding(
+                          padding: EdgeInsets.all(5.0),
+                          child: Image.asset(
+                            'assets/images/logo.png',
+                            width: 80,
+                            height: 80,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('TradeSense',style: TextStyle(color: Colors.white,fontSize: 20.0,),),
+                      )
+                    ],
+                  ),
+                )),
+            CustomListTitle(Icons.home, 'Accueil', () => {}),
+            CustomListTitle(Icons.history, 'Accualité', () => {}),
+            CustomListTitle(Icons.perm_media, 'Media', () => {}),
+            CustomListTitle(Icons.exit_to_app, 'Quitter', () => {})
+          ],
+        ),
       ),
       body: Column(
-        children: <Widget>[
-          HomeScreenTopPart()
-        ],
+        children: <Widget>[HomeScreenTopPart()],
       ),
     );
   }
 }
+
 class HomeScreenTopPart extends StatefulWidget {
   @override
   _HomeScreenTopPartState createState() => _HomeScreenTopPartState();
@@ -31,11 +68,14 @@ class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
     return Stack(
       children: <Widget>[
         ClipPath(
-          clipper: CustomShapeClipper() ,
-           child: Container(height: 450.0, color: Color.fromRGBO(67, 90, 177, 1.0),
-          )
-        )
+            clipper: CustomShapeClipper(),
+            child: Container(
+              height: 450.0,
+              color: Color.fromRGBO(67, 90, 177, 1.0),
+            ))
       ],
     );
   }
 }
+
+
