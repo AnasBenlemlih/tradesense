d4237e9d9bd91a24a2d4ff4501d73cd52118e294
IKRAM <ikrambouleksaks@gmail.com>
2020-05-11 16:04:56 +0200

    Ajout des fichiers Home

--- tradesense/lib/home.dart
+++ tradesense/lib/home.dart
@@ -1,16 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'package:tradesense/topPartHome.dart';
 import 'CustomShapeClipper.dart';
 import 'CustomListTitle.dart';
 
 class HomeScreen extends StatelessWidget {
+  //String current ="Régime";
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromRGBO(67, 90, 177, 1.0),
-        title: Text('TradeSense'),
+        elevation: 0,
       ),
       drawer: Drawer(
         child: ListView(
@@ -50,32 +53,158 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
       ),
-      body: Column(
-        children: <Widget>[HomeScreenTopPart()],
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+          HomeScreenTopPart(),
+          SizedBox(height: 20),
+            PreventCard(
+              text:
+              "Accéder à la base de recherche \n  des accords commerciaux.",
+              image: "assets/images/icon1.png",
+              title: "Accords commerciaux",
+            ),
+            PreventCard(
+              text:
+              "Accès au référentiel de mesures\nsanitaires et phytosanitaires.",
+              image: "assets/images/icon3.png",
+              title: "Mesures sanitaires et \nphytosanitaires",
+            ),
+            PreventCard(
+              text:
+              "Accédez au référentiel des \n procedédures.",
+              image: "assets/images/icon2.png",
+              title: "Procédures",
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+class OptionCard extends StatelessWidget {
+  final String image;
+  final String title;
+  final String title2;
+  final bool isActive;
+  const OptionCard({
+    Key key,
+    this.image,
+    this.title,
+    this.title2,
+    this.isActive = false,
+  }) : super(key: key);
 
-class HomeScreenTopPart extends StatefulWidget {
-  @override
-  _HomeScreenTopPartState createState() => _HomeScreenTopPartState();
-}
-
-class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        ClipPath(
-            clipper: CustomShapeClipper(),
-            child: Container(
-              height: 450.0,
-              color: Color.fromRGBO(67, 90, 177, 1.0),
-            ))
-      ],
+    return Container(
+      padding: EdgeInsets.all(40),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.white,
+        boxShadow: [
+          isActive
+              ? BoxShadow(
+            offset: Offset(0, 10),
+            blurRadius: 20,
+            color: Colors.grey,
+          )
+              : BoxShadow(
+            offset: Offset(0, 3),
+            blurRadius: 6,
+            color: Colors.grey,
+          ),
+        ],
+      ),
+      child: Column(
+        children: <Widget>[
+          Image.asset(image, height: 50),
+          Text(
+            title,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Text(
+            title2,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
     );
   }
 }
+class PreventCard extends StatelessWidget {
+  final String image;
+  final String title;
+  final String text;
+  const PreventCard({
+    Key key,
+    this.image,
+    this.title,
+    this.text,
+  }) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: SizedBox(
+        height: 156,
+        child: Stack(
+          alignment: Alignment.centerLeft,
+          children: <Widget>[
+            Container(
+              height: 136,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
+                //la couleur du box qui contient l'option
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(3, 3),
+                    blurRadius: 10,
+                    color: Colors.grey,
+                  ),
+
+                ],
+              ),
+              child: Row(
+                children: <Widget>[
+                  Image.asset(image),
+                  Column(
+                    children: <Widget>[
+                      SizedBox(height: 30),
+                      Text(
+                        title,
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        text,
+                        maxLines: 4,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: 60),
+                FloatingActionButton(
+                      backgroundColor: Colors.amberAccent,
+                      onPressed: (){},
+                      //  tooltip: 'Increment',
+                      child: Icon(Icons.arrow_forward),
+                    ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
